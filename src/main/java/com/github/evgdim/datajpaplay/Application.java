21b2f810257d9ca96d3729609d1ddfe2fe00132f
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2017-04-29 22:16:17 +0300

    moved main logic to tests

--- src/main/java/com/github/evgdim/datajpaplay/Application.java
+++ src/main/java/com/github/evgdim/datajpaplay/Application.java
@@ -1,45 +1,11 @@
 package com.github.evgdim.datajpaplay;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.github.evgdim.datajpaplay.entity.Customer;
-import com.github.evgdim.datajpaplay.entity.CustomerCategory;
-import com.github.evgdim.datajpaplay.reposiotry.CustomerCategoryRepository;
-import com.github.evgdim.datajpaplay.reposiotry.CustomerRepository;
 
 @SpringBootApplication
 public class Application {
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
-
-	@Bean
-	public CommandLineRunner demo(CustomerRepository customerRepo, CustomerCategoryRepository categoryRepo) {
-		return (args) -> {
-			CustomerCategory customerCategory = categoryRepo.save(new CustomerCategory("standart"));
-			//CustomerCategory findOne = categoryRepo.findOne(1L);
-			Customer cust = new Customer("evgeni", "Dimitrov", customerCategory);
-			
-			customerRepo.save(cust);
-			
-			customerRepo.updateNameByIds("newName", Arrays.asList(cust.getId()));
-			
-			customerRepo.findAll().forEach(c -> log.info(c.toString()));
-			
-			customerRepo.deleteByCategoryIdSession(customerCategory.getId());
-			
-			customerRepo.findAll().forEach(c -> log.info(c.toString()));
-		};
-	}
-
 }
--- src/test/java/com/github/evgdim/datajpaplay/CustomerRepositoryTests.java
+++ src/test/java/com/github/evgdim/datajpaplay/CustomerRepositoryTests.java
@@ -1,11 +1,15 @@
 package com.github.evgdim.datajpaplay;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -14,25 +18,50 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.evgdim.datajpaplay.entity.Customer;
+import com.github.evgdim.datajpaplay.entity.CustomerCategory;
+import com.github.evgdim.datajpaplay.reposiotry.CustomerCategoryRepository;
 import com.github.evgdim.datajpaplay.reposiotry.CustomerRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class CustomerRepositoryTests {
+	private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryTests.class);
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
-    private CustomerRepository customers;
+    private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerCategoryRepository customerCategoryRepository;
 
     @Test
     public void testFindByLastName() {
         Customer customer = new Customer("evgeni","Dimitrov",null);
         entityManager.persist(customer);
 
-        List<Customer> findByLastName = customers.findByLastName(customer.getLastName());
+        List<Customer> findByLastName = customerRepository.findByLastName(customer.getLastName());
 
         assertThat(findByLastName).extracting(Customer::getLastName).containsOnly(customer.getLastName());
+    }
+    
+    @Test
+    public void testDeleteById() {
+    	CustomerCategory customerCategory = customerCategoryRepository.save(new CustomerCategory("standart"));
+		Customer cust = new Customer("evgeni", "Dimitrov", customerCategory);
+		customerRepository.save(cust);
+		assertEquals(1L, customerRepository.findAll().spliterator().getExactSizeIfKnown());
+		customerRepository.deleteByCategoryIdSession(customerCategory.getId());
+		assertEquals(0L, customerRepository.findAll().spliterator().getExactSizeIfKnown());
+    }
+    
+    @Test
+    public void updateNameByIdsTest() {
+    	CustomerCategory customerCategory = customerCategoryRepository.save(new CustomerCategory("standart"));
+    	Customer cust = new Customer("evgeni", "Dimitrov", customerCategory);
+		customerRepository.save(cust);
+    	customerRepository.updateNameByIds("newName", Arrays.asList(cust.getId()));
+    	entityManager.refresh(cust);
+    	assertEquals("updated name matches", "newName", cust.getFirstName());
     }
 }
